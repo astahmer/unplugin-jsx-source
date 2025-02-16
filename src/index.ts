ab4354dbe0219710597f5cb1516e2f758e63bbd6
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2025-02-16 12:48:52 +0100

    feat: options

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,9 @@ import _generate from '@babel/generator';
 import * as t from '@babel/types';
 
 import type { UnpluginFactory } from 'unplugin'
-import type { Options } from './types'
+import { createFilter } from 'unplugin-utils'
 import { createUnplugin } from 'unplugin'
+import { resolveOption,Options } from './options';
 
 // @ts-expect-error
 const traverse = (_traverse.default || _traverse) as any as typeof _traverse;
@@ -13,45 +14,48 @@ const traverse = (_traverse.default || _traverse) as any as typeof _traverse;
 const generate = (_generate.default || _generate) as any as typeof _generate;
 
 
-export const unpluginFactory: UnpluginFactory<Options | undefined> = options => ({
-  name: 'unplugin-jsx-source',
-  transformInclude(id) {
-    return id.endsWith('main.ts')
-  },
-  transform(code, id) {
-    if (!id.endsWith('.jsx') && !id.endsWith('.tsx')) {
-      return null;
-    }
+export const unpluginFactory: UnpluginFactory<Options | undefined> = (options = {}) => {
+  const opt = resolveOption(options)
+  const filter = createFilter(opt.include, opt.exclude)
 
-    const ast = parse(code, {
-      sourceType: 'module',
-      plugins: ['jsx', 'typescript'],
-    });
+  return ({
+    name: 'unplugin-jsx-source',
+    enforce: opt.enforce,
+    transformInclude(id) {
+      return filter(id)
+    },
+    transform(code, id) {
+      const ast = parse(code, {
+        sourceType: 'module',
+        plugins: ['jsx', 'typescript'],
+        ...opt.parserOptions,
+      });
 
-    traverse(ast, {
-      JSXOpeningElement(path) {
-        if (path.node.name.type === "JSXIdentifier" && path.node.loc) {
-          const filename = id.split('/').pop();
+      traverse(ast, {
+        JSXOpeningElement(path) {
+          if (path.node.name.type === "JSXIdentifier" && path.node.loc) {
+            const filename = id.split('/').pop();
 
-          // Add `data-at` attribute with filename and location
-          const dataAtAttr = t.jsxAttribute(
-            t.jsxIdentifier('data-at'),
-            t.stringLiteral(`${filename}:${path.node.loc.start.line}-${path.node.loc.end.line}`)
-          );
+            // Add `data-at` attribute with filename and location
+            const dataAtAttr = t.jsxAttribute(
+              t.jsxIdentifier('data-at'),
+              t.stringLiteral(`${filename}:${path.node.loc.start.line}-${path.node.loc.end.line}`)
+            );
 
-          path.node.attributes.unshift(dataAtAttr);
-        }
-      },
-    });
+            path.node.attributes.unshift(dataAtAttr);
+          }
+        },
+      });
 
-    const { code: transformedCode } = generate(ast, { filename: id}, code);
+      const { code: transformedCode } = generate(ast, { filename: id }, code);
 
-    return {
-      code: transformedCode,
-      map: null,
-    };
-  },
-})
+      return {
+        code: transformedCode,
+        map: null,
+      };
+    },
+  });
+}
 
 export const unplugin = /* #__PURE__ */ createUnplugin(unpluginFactory)
 
---
+++ src/options.ts
@@ -0,0 +1,28 @@
+import type { ParserOptions } from '@babel/parser'
+import type { FilterPattern } from 'unplugin-utils'
+
+export interface Options {
+  include?: FilterPattern
+  exclude?: FilterPattern | undefined
+  enforce?: 'post' | 'pre' | undefined
+  parserOptions?: ParserOptions
+}
+
+type Overwrite<T, U> = Pick<T, Exclude<keyof T, keyof U>> & U
+
+export type OptionsResolved = Overwrite<
+  Required<Options>,
+  {
+    exclude: Options['exclude']
+    enforce: Options['enforce']
+  }
+>
+
+export function resolveOption(options: Options): OptionsResolved {
+  return {
+    include: options.include || [/\.[jt]sx?$/],
+    exclude: options.exclude || undefined,
+    enforce: options.enforce || undefined,
+    parserOptions: options.parserOptions || {},
+  }
+}
