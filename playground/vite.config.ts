bc6fdeddb415e9c33c78c5eb71c470e9b99db3ed
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2025-02-16 12:40:45 +0100

    fix: TypeError: (0 , import_vite_plugin_inspect.default) is not a function

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -3,6 +3,7 @@ import Inspect from 'vite-plugin-inspect'
 import react from '@vitejs/plugin-react-swc'
 import Unplugin from '../src/vite'
 
+
 export default defineConfig({
   plugins: [
     Inspect(),
